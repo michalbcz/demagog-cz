13b3ad2b74173786343574ee1e65caf5049080cf
Vlastimil Dolejs <vlasta.dolejs@gmail.com>
2012-11-24 15:11:40 +0100

    mock data

--- demagog/app/Global.java
+++ demagog/app/Global.java
@@ -1,5 +1,7 @@
 import java.lang.reflect.Field;
 
+import models.Quote;
+
 import com.google.code.morphia.logging.MorphiaLoggerFactory;
 import com.google.code.morphia.logging.slf4j.SLF4JLogrImplFactory;
 
@@ -19,6 +21,32 @@ public class Global extends GlobalSettings {
 		}
 		
 		MorphiaLoggerFactory.registerLogger(SLF4JLogrImplFactory.class);
+		
+		initDeveloperData();
+	}
+	
+	private void initDeveloperData() {
+		Quote.deleteAll();
+		
+		new Quote(
+				"I po tomto daňovém balíčku, který byl tedy napodruhé prosazen, bude daňová zátěž o 2% daňové kvóty nižší, než byla za pana ministra Sobotky.",
+				"http://demagog.cz/diskusie/76/trvale-udrzitelna-krize-miroslav-kalousek", 
+				"Miroslav Kalousek").save();
+
+		new Quote(
+				"My jsme zdědili deficit po Fischerově vládě ve výši 5,8% HDP.",
+				"http://demagog.cz/diskusie/76/trvale-udrzitelna-krize-miroslav-kalousek", 
+				"Miroslav Kalousek").save();
+
+		new Quote(
+				"Každý správce programu (operačního programu, čerpajícího evropské fondy, pozn.), máme jich bohužel strašně moc, máme jich 24, má svého auditora, který provádí kontrolu, a to bývá většinou podřízený toho správce programu.",
+				"http://demagog.cz/diskusie/76/trvale-udrzitelna-krize-miroslav-kalousek", 
+				"Jiří Paroubek").save();
+
+		new Quote(
+				"Ale ten ekonomický mechanismus (převodu finančních prostředků státního podniku do státního rozpočtu, pozn.) tam neexistuje a to právě je předmětem teď novely zákona o státním podniku, který už je ve sněmovně a který má právě tento transfer zajistit.",
+				"http://demagog.cz/diskusie/76/trvale-udrzitelna-krize-miroslav-kalousek", 
+				"Jiří Oliva").save();
 	}
 	
 }
