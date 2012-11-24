1aea344bcbe201506d68cf5e21fece5f2813cc42
Vlastimil Dolejs <vlasta.dolejs@gmail.com>
2012-11-24 14:52:41 +0100

    quotes list page

--- demagog/app/controllers/Application.java
+++ demagog/app/controllers/Application.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import java.util.List;
+
+import models.Quote;
 import play.*;
 import play.mvc.*;
 
@@ -7,8 +10,13 @@ import views.html.*;
 
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	} 
   
+	public static Result showQuotes() {
+		List<Quote> quotes = Quote.findAll();
+		
+		return ok(quotes_list.render(quotes));
+	}
 }
