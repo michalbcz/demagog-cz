d35a0a56e797c48312de43df6762b0fd4fdd6736
Martin Wenisch <marwen@seznam.cz>
2012-11-24 15:22:10 +0100

    controller

--- demagog/app/controllers/Application.java
+++ demagog/app/controllers/Application.java
@@ -1,14 +1,25 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
+import models.Quote;
+import play.mvc.Controller;
+import play.mvc.Result;
 import views.html.*;
+import play.data.*;
 
-public class Application extends Controller {
-  
+public class Application extends Controller {	
+	
   public static Result index() {
     return ok(index.render("Your new application is ready."));
   }
   
+  public static Result submit() {
+  	Form<Quote> submitForm = form(Quote.class);
+  	Quote quote = submitForm.bindFromRequest().get();
+
+  	quote.save();
+  	
+  	return ok(submitResponse.render(""));
+  }
+
+
 }
