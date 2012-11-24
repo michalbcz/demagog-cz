b2ea3fef57a71eaebf44ea4f5253091d4635dd4e
Vlastimil Dolejs <vlasta.dolejs@gmail.com>
2012-11-24 11:48:13 +0100

    morphia dependency

--- demagog/project/Build.scala
+++ demagog/project/Build.scala
@@ -8,9 +8,9 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+    	"com.google.code" % "morphia" % "0.91"
     )
-
+    
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
     )
