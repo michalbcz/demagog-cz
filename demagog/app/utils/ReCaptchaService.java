1421c1c233ce6b5b9109633952d2213457f623e8
Jan Krakora <jan.krakora.cz@gmail.com>
2013-05-12 09:01:45 +0200

    fixed #44 - Provide default recaptcha keys for developing

--- demagog/app/utils/ReCaptchaService.java
+++ demagog/app/utils/ReCaptchaService.java
@@ -1,9 +1,13 @@
 package utils;
 
+import java.util.Properties;
+
 import net.tanesha.recaptcha.ReCaptchaFactory;
 import net.tanesha.recaptcha.ReCaptchaResponse;
 
-import java.util.Properties;
+import org.springframework.util.Assert;
+
+import play.Play;
 
 /**
  *
@@ -13,6 +17,26 @@ import java.util.Properties;
 public class ReCaptchaService {
 
     private net.tanesha.recaptcha.ReCaptcha reCaptchaService;
+    
+    /**
+     * The public development key. For development purposes only!
+     */
+    private final static String PUBLIC_DEVELOPMENT_KEY = "6LcePuESAAAAACXN6NSveoRs29nYU4_0TOZLUuLM";
+    
+    /**
+     * The private development key. For development purposes only!
+     */
+    private final static String PRIVATE_DEVELOPMENT_KEY = "6LcePuESAAAAAKC9wsaPA_O5IB4mihZYOsJCL8Ca";
+    
+    /**
+     * System property to get public recaptcha key
+     */
+    private final static String PUBLIC_KEY_PROPERTY = "demagog.recaptcha.publicKey";
+    
+    /**
+     * System property to get private recaptcha key
+     */
+    private final static String PRIVATE_KEY_PROPERTY = "demagog.recaptcha.privateKey";
 
     private final static String CREATE_API_KEYS_ERROR_MESSAGE =
                                     "For that key you need to create account on http://www.google.com/recaptcha";
@@ -22,18 +46,38 @@ public class ReCaptchaService {
     }
 
     private ReCaptchaService() {
-        String publicKey = systemPropertyOrError("demagog.recaptcha.publicKey", CREATE_API_KEYS_ERROR_MESSAGE);
-        String privateKey = systemPropertyOrError("demagog.recaptcha.privateKey", CREATE_API_KEYS_ERROR_MESSAGE);
+        String publicKey = getKeyFromSystemProperty(PUBLIC_KEY_PROPERTY, PUBLIC_DEVELOPMENT_KEY);
+        String privateKey = getKeyFromSystemProperty(PRIVATE_KEY_PROPERTY, PRIVATE_DEVELOPMENT_KEY);
         reCaptchaService = ReCaptchaFactory.newReCaptcha(publicKey, privateKey, /* include non script */ false);
+    }
+    
+    private String getKeyFromSystemProperty(String systemPropertyName, String defaultValue) {
+    	if (Play.isProd()) {
+    		return systemPropertyOrError(systemPropertyName, CREATE_API_KEYS_ERROR_MESSAGE);
+    	} else {
+    		return systemPropertyOrDefault(systemPropertyName, defaultValue);
+    	}
+    }
+    
+    private String systemPropertyOrDefault(String systemPropertyName, String defaultValue) {
+    	Assert.notNull(systemPropertyName);
+        
+    	String systemPropertyValue = System.getProperty(systemPropertyName);
+
+        if (systemPropertyValue == null) {
+        	systemPropertyValue = defaultValue;
+        }
+        return systemPropertyValue;
     }
 
     private String systemPropertyOrError(String systemPropertyName, String errorMessage) {
-        String systemPropertyValue = System.getProperty(systemPropertyName);
+    	Assert.notNull(systemPropertyName);
+        
+    	String systemPropertyValue = System.getProperty(systemPropertyName);
 
         if (systemPropertyValue == null) {
             throw new RuntimeException("System property '" + systemPropertyName + "' must be set. " + errorMessage);
         }
-
         return systemPropertyValue;
     }
 
