0e1f114da1365caecf83133921ef6c44f15929dc
Vlastimil Dolejs <vlasta.dolejs@gmail.com>
2013-10-30 21:19:36 +0100

    resolved #67 - Better visualisation of verified on demagog.cz state

--- demagog/app/utils/LabelProvider.java
+++ demagog/app/utils/LabelProvider.java
@@ -5,40 +5,40 @@ import models.Quote.QuoteState;
 import org.springframework.util.Assert;
 
 public class LabelProvider {
-	
+
 	private LabelProvider() {}
-	
+
 	public static String getDescription(QuoteState state) {
 		Assert.notNull(state);
 
 		switch (state) {
 			case ANALYSIS_IN_PROGRESS:
 				return "Výrok získal dostatečný počet hlasů a je ověřován.";
-			
+
 			case APPROVED_FOR_VOTING:
 				return "Pro výrok lze hlasovat";
-				
+
 			case NEW:
 				return "Nově přidaný výrok, který musí být schválen k hlasování.";
 
             case CHECKED_AND_PUBLISHED:
-            	return "Výrok byl ověřen a je publikován.";
+            	return "Výrok byl ověřen a je publikován na demagog.cz";
 
 		    default:
 			    throw new IllegalArgumentException("Neznamy stav: " + state);
 		}
 	}
-	
+
 	public static String getLabel(QuoteState state) {
 		Assert.notNull(state);
 
 		switch (state) {
 			case ANALYSIS_IN_PROGRESS:
 				return "Ověřuje se";
-			
+
 			case APPROVED_FOR_VOTING:
 				return "V hlasování";
-				
+
 			case NEW:
 				return "Nový";
 
