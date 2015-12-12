7e25a2fbdb2f23a6cf9adfdb59ecd2b645ef76f5
Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>
2015-12-12 21:39:56 +0100

    openshift deployment stuff

--- deploy_to_openshift.bat
+++ deploy_to_openshift.bat
@@ -10,17 +10,15 @@ cd ..
 echo [info] removing old deploy repo
 rd /S /Q ..\demagog-openshift-deploy
 
-echo "[info] cloning deploy repo"
+echo "[info] init new deploy repo"
 md ..\demagog-openshift-deploy
 pushd ..\demagog-openshift-deploy
-git clone ssh://566c2e6589f5cffb1900004b@overto-demagogcz.rhcloud.com/~/git/overto.git/ .
+git init .
+git remote add openshift ssh://566c2e6589f5cffb1900004b@overto-demagogcz.rhcloud.com/~/git/overto.git/
 
-echo "[info] removing old deploy repo contents"
-for %%i in (*) do del %%i
-for /D %%i in (*) do if not %%i == .git rmdir /S /Q %%i
 popd
 
-echo "[info] copying new deploy repo contents"
+echo "[info] copying new deploy repo content"
 md ..\demagog-openshift-deploy\demagog
 md ..\demagog-openshift-deploy\.openshift
 xcopy demagog\*.* ..\demagog-openshift-deploy\demagog /H /S /E /K /Q
@@ -30,6 +28,13 @@ echo "[info] remove .gitignores"
 del /s .gitignore
 cd ..\demagog-openshift-deploy
 
+echo "[info] running: git add . -A"
+git add . -A
+
+echo "[info] running: git commit -m \"deployment\""
+git commit -m "deployment"
+
+echo "[info] make action hooks executable"
 git update-index --chmod=+x .openshift/action_hooks/build
 git update-index --chmod=+x .openshift/action_hooks/deploy
 git update-index --chmod=+x .openshift/action_hooks/load_config
@@ -37,12 +42,10 @@ git update-index --chmod=+x .openshift/action_hooks/post_deploy
 git update-index --chmod=+x .openshift/action_hooks/pre_build
 git update-index --chmod=+x .openshift/action_hooks/start
 git update-index --chmod=+x .openshift/action_hooks/stop
-
-echo "[info] running: git add . -A"
 git add . -A
+git commit -m "action hooks executable"
 
-echo "[info] running: git commit -m \"deployment\""
-git commit -m "deployment"
+echo "[info] running: git push openshift"
+git push openshift --force
 
-echo "[info] running: git push origin"
-git push origin
+popd
